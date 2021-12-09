ee36a68c2ec0c249337916c5673e11c74329dadd
swathi.moorthy@microchip.com <swathi.moorthy@microchip.com>
2021-12-09 09:53:35 +0000

    Project created

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=dspic33ck.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/dspic33ck.production.hex
